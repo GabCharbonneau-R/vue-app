a7b3548fb7261b84b81fc58b2645ce62c0805819
GabCharbonneau-R <gcharbonneau@cstj.qc.ca>
2024-09-03 11:41:20 -0400

    app-demo-live

---
+++ src/components/Cards.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+      <img :src="image" :alt="title" v-if="image">
+      <h3>{{ title }}</h3>
+      <p>{{ content }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      title: String,
+      content: String,
+      image: String
+    }
+  };
+  </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h1>Counter</h1>
+      <p>Count: {{ count }}</p>
+      <button @click="increment">Increment</button>
+      <button @click="decrement">Decrement</button>   
+  
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        count: 0
+      };
+    },
+    methods: {
+      increment() {
+        this.count++;
+      },
+      decrement() {
+        this.count--; 
+  
+      }
+    }
+  };
+  </script>
